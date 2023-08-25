63d617d2b544c4525138c7bc27e2ac87e6cf89b0
Electronic12 <sowgatfilm@gmail.com>
2023-08-25 23:59:10 +0500

    committt

--- src/app.go
+++ src/app.go
@@ -43,6 +43,8 @@ func set_routers(router *gin.Engine) {
 	create_router(router, "customer", customer.Controller)
 	create_router(router, "brands", brands.Controller)
 	create_router(router, "users", users.Controller)
+	// create_router(router, "users", users.Controller)
+	// create_router(router, "users", users.Controller)
 }
 
 func create_router(
